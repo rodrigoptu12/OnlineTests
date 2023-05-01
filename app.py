c96f21a6e9e07726f578973de55143b1bab63330
Rodrigo Pereira Couto <rodrigopereiracouto@hotmail.com>
2023-05-01 14:19:31 -0300

    Implemented PostgreSQL with Flask SQLAlchemy and created users table

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import pyrebase
 import os
 from dotenv import load_dotenv
@@ -6,6 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rodrigoptu12:123456@localhost/attendancelist'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 firebase_api_key = os.getenv("FIREBASE_API_KEY")
 firebase_auth_domain = os.getenv("FIREBASE_AUTH_DOMAIN")
@@ -35,6 +40,8 @@ app.secret_key = 'secret'
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    with app.app_context():
+        db.create_all()
     if ("user" in session):
         print(session['user'])
         return render_template('home.html', email=session['user']['email'])
@@ -44,6 +51,13 @@ def index():
         try:
             user = auth.sign_in_with_email_and_password(email, password)
             session['user'] = user
+            usuario_existente = Usuario.query.filter_by(email=email).first()
+            if usuario_existente:
+                return redirect('/')
+            else:
+                novo_usuario = Usuario(nome=email, email=email)
+                db.session.add(novo_usuario)
+                db.session.commit()
             return redirect('/')
         except:
             return "Invalid email or password"
@@ -73,6 +87,42 @@ def logout():
         return redirect('/')
     session.pop('user')
     return redirect('/')
+
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'<Usuario {self.nome}>'
+
+
+# db.create_all()
+
+
+@app.route('/adicionar_usuario', methods=['POST'])
+def adicionar_usuario():
+    nome = request.form['nome']
+    email = request.form['email']
+    novo_usuario = Usuario(nome=nome, email=email)
+    db.session.add(novo_usuario)
+    db.session.commit()
+    return f'Usuário {nome} adicionado com sucesso'
+
+
+@app.route('/usuarios', methods=['GET'])
+def listar_usuarios():
+    usuarios = Usuario.query.all()
+    usuarios_json = []
+    for usuario in usuarios:
+        usuario_json = {
+            'id': usuario.id,
+            'nome': usuario.nome,
+            'email': usuario.email
+        }
+        usuarios_json.append(usuario_json)
+    return jsonify(usuarios_json)
 
 
 if __name__ == '__main__':
